7da962d4013cb67450a279916a2bfc65feed4538
rootIV <vitordevcontato@gmail.com>
2025-09-28 23:51:41 -0300

    feat: phone viewport
    Signed-off-by: rootIV <vitordevcontato@gmail.com>

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ import "../styles/components/hero.scss"
 export default function HeroElement() {
   return (
     <div className="main">
-      <h1>🚀 Bem-vindo à Beetomation 🚀</h1>
+      <h1>Bem-vindo à Beetomation</h1>
       <p>A melhor ferramenta do mercado de apostas automatizadas!</p>
     </div>
   );
